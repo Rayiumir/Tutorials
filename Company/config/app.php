2f620c9cd1e39fe7803da837b9077d55885dd29a
Rayiumir <swordart2050@gmail.com>
2025-08-14 02:50:17 +0330

    Reinstall Laravel 12

--- Company/config/app.php
+++ Company/config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Tehran',
+    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'fa'),
+    'locale' => env('APP_LOCALE', 'en'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
